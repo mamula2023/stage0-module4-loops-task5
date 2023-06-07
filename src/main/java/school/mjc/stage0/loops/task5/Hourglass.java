1c78f62f6f2334f3afdb708226e121a478f3d52e
mamula2023 <mamulashvili.2017@gmail.com>
2023-06-07 16:42:49 +0400

    graphics

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,49 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int width = height;
+        for (int i = 0; i < height / 2; i++) {
+            int offset = (height - width) / 2;
+            for (int j = 0; j < offset; j++) {
+                System.out.print(' ');
+            }
+            for (int j = 0; j < width; j++)
+                System.out.print(8);
+            for (int j = 0; j < offset; j++)
+                System.out.print(' ');
+            System.out.println();
+
+
+            width -= 2;
+        }
+
+        if (height % 2 != 0) {
+           int offset = (height - width) / 2;
+            for (int j = 0; j < offset; j++) {
+                System.out.print(' ');
+            }
+            for (int j = 0; j < width; j++)
+                System.out.print(8);
+            for (int j = 0; j < offset; j++)
+                System.out.print(' ');
+            System.out.println();
+            width+=2;
+        }else width+=2;
+        for (int i = 0; i < height / 2; i++) {
+            int offset = (height - width) / 2;
+            for (int j = 0; j < offset; j++) {
+                System.out.print(' ');
+            }
+            for (int j = 0; j < width; j++)
+                System.out.print(8);
+            for (int j = 0; j < offset; j++)
+                System.out.print(' ');
+            System.out.println();
+
+
+            width += 2;
+        }
+
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        for(int i = 0; i<cathetusLength; i++){
+            if(i == 0||i == cathetusLength-1){
+                for(int j = 0; j<=i; j++){
+                    System.out.print(8);
+                }
+                System.out.println();
+            }else{
+                System.out.print(8);
+                for(int j = 0; j<i-1; j++)
+                    System.out.print(8);
+                System.out.println(8);
+            }
+        }
     }
 }
